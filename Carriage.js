2bb003629d29ecbfa0570fbec0ec315eb8973d3e
Soham733 <59410256+Soham733@users.noreply.github.com>
2020-07-14 20:10:57 +0530

    Add files via upload

---
+++ Carriage.js
@@ -0,0 +1,20 @@
+class Carriage {
+    constructor(x,y,width,height){
+     var options={
+         isStatic:true
+ }
+     this.body=Bodies.rectangle(x,y,width,height,options);
+     this.width=width;
+     this.height=height;
+     World.add(world,this.body);
+     this.image=loadImage("Images/Carriage.png");
+
+        
+    }
+    display(){
+        imageMode(CENTER);
+        image(this.image, this.body.position.x, this.body.position.y,this.width,this.height);
+        this.body.velocity.y=2;
+    }
+     
+}
